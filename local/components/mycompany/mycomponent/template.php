a8aaa4de58d66d4c64c73a543ab607c1839799c2
gitName <bartk@list.ru>
2025-01-17 22:27:33 +0300

    feat: the button onclick behaviour: toggle eventListener, delete eventListener

--- local/components/mycompany/mycomponent/template.php
+++ local/components/mycompany/mycomponent/template.php
@@ -7,11 +7,57 @@
             <?php foreach ($arResult["TASKS"] as $task): ?>
                 <li>
                     <?php echo htmlspecialchars($task["task"]) ?>
-                    <button><?php echo $task["is_completed"] ? "Again to do" : "Done" ?></button>
-                    <button>Delete</button>
+                    <button id="toggleBtn" data-action="toggle"
+                        data-task-id="<?php echo $task['id']; ?>"><?php echo $task["is_completed"] ? "To do again" : "Done" ?></button>
+                    <button id="deleteBtn" data-action="delete" data-task-id="<?php echo $task['id']; ?>">Delete</button>
                 </li>
             <?php endforeach; ?>
         </ul>
         <h1><?php echo $arResult["MESSAGE"]; ?></h1>
     <?php endif; ?>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function () {
+        $('#toggleBtn').on("click", function () {
+            const taskId = $(this).data('task-id');
+
+            $.ajax({
+                url: "/ajax.php",
+                method: "POST",
+                data: { id: taskId },
+                success: function (response) {
+                    if (response.success) {
+                        location.reload();
+                    } else {
+                        console.error("Error: ", error);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.error("Error: ", error)
+                }
+            })
+        });
+        $('#deleteBtn').on("click", function () {
+            const taskId = $(this).data("task-id");
+
+            $.ajax({
+                url: "/ajax.php",
+                method: "DELETE",
+                data: { id: taskId },
+                success: function (response) {
+                    if (response.success) {
+                        location.reload();
+                    } else {
+                        console.error("Error: ", error);
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.error("Error: ", error)
+                }
+            })
+        })
+    })
+</script>
